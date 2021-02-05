fd80b84227860835151f8a7a6b3082d3716026cb
Sudhanshu Dixit <shudhanshu.dixit_cs18@gla.ac.in>
2021-02-05 19:57:25 +0530

    initial

---
+++ power.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class power {
+    
+
+        public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int i=sc.nextInt();
+   int j=sc.nextInt();
+           
+            double result = Math.pow(i, j);
+    
+            System.out.println("Answer = " + result);
+        }
+    }
+    
+
